06508e5a2d7248bea456b8a3f837bc50239df18c
aj-bhagat <ajaybhagatm620@gmail.com>
2018-10-18 02:28:36 +0530

    Node added

---
+++ src/node/db.js
@@ -0,0 +1,9 @@
+const mongoose= require('mongoose');
+mongoose.connect('mongodb://localhost:27017/article',{ useNewUrlParser: true },(err)=>{
+    if(!err)
+        console.log('MongoDB connection succeeded.');
+    else
+        console.log('Error in DB connection : '+JSON.stringify(err,undefined,2));
+});
+
+module.exports=mongoose;
